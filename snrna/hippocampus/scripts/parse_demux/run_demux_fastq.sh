25d0ec3e42f4890c7d3afada6f180e7741b0ca69
Liz Rebboah <erebboah@uci.edu>
2023-11-27 14:47:52 -0800

    hippocampus demux stuff

---
+++ snrna/hippocampus/scripts/parse_demux/run_demux_fastq.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+#SBATCH --job-name=splitfastq    ## Name of the job
+#SBATCH -A SEYEDAM_LAB            ## account to charge 
+#SBATCH -p standard               ## partition/queue name
+#SBATCH --mem=256G
+#SBATCH --output=fastqsplit-%J.out ## output log file
+#SBATCH --error=fastqsplit-%J.err ## error log file
+#SBATCH -n 32
+
+source ~/miniconda3/bin/activate seqtkpython3
+
+gunzip ../../fastq/HC_12kA_R*.fastq.gz
+gunzip ../../analysis_12A/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_12A/ \
+  -cell_meta ../../analysis_12A/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_12A/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_12kA_R1.fastq \
+  -f2 ../../fastq/HC_12kA_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_12kA_R*.fastq
+gzip ../../analysis_12A/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_12kB_R*.fastq.gz
+gunzip ../../analysis_12B/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_12B/ \
+  -cell_meta ../../analysis_12B/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_12B/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_12kB_R1.fastq \
+  -f2 ../../fastq/HC_12kB_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_12kB_R*.fastq
+gzip ../../analysis_12B/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_12kC_R*.fastq.gz
+gunzip ../../analysis_12C/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_12C/ \
+  -cell_meta ../../analysis_12C/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_12C/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_12kC_R1.fastq \
+  -f2 ../../fastq/HC_12kC_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_12kC_R*.fastq
+gzip ../../analysis_12C/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_12kD_R*.fastq.gz
+gunzip ../../analysis_12D/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_12D/ \
+  -cell_meta ../../analysis_12D/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_12D/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_12kD_R1.fastq \
+  -f2 ../../fastq/HC_12kD_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_12kD_R*.fastq
+gzip ../../analysis_12D/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_12kE_R*.fastq.gz
+gunzip ../../analysis_12E/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_12E/ \
+  -cell_meta ../../analysis_12E/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_12E/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_12kE_R1.fastq \
+  -f2 ../../fastq/HC_12kE_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_12kE_R*.fastq
+gzip ../../analysis_12E/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_2kA_R*.fastq.gz
+gunzip ../../analysis_2A/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../standard_2A/ \
+  -cell_meta ../../analysis_2A/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_2A/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_2kA_R1.fastq \
+  -f2 ../../fastq/HC_2kA_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_2kA_R*.fastq
+gzip ../../analysis_2A/process/single_cells_barcoded_head.fastq
+
+############
+
+gunzip ../../fastq/HC_2kB_R*.fastq.gz
+gunzip ../../analysis_2B/process/single_cells_barcoded_head.fastq.gz
+  
+python demux_fastqs.py \
+  -o ../../lr_match_2B/ \
+  -cell_meta ../../analysis_2B/all-well/DGE_unfiltered/cell_metadata.csv \
+  -sp_fastq ../../analysis_2B/process/single_cells_barcoded_head.fastq \
+  -f1 ../../fastq/HC_2kB_R1.fastq \
+  -f2 ../../fastq/HC_2kB_R2.fastq \
+  -t 32
+
+gzip ../../fastq/HC_2kB_R*.fastq
+gzip ../../analysis_2B/process/single_cells_barcoded_head.fastq
---
+++ snrna/hippocampus/scripts/parse_demux/splitpipe_2B.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#SBATCH --job-name=spipe2B   ## Name of the job
+#SBATCH -A seyedam_lab            ## account to charge 
+#SBATCH -p standard               ## partition/queue name
+#SBATCH --nodes=1                 ## (-N) number of nodes to use
+#SBATCH --cpus-per-task=16         ## number of cores the job needs
+#SBATCH --output=spipe2B-%J.out ## output log file
+#SBATCH --error=spipe2B-%J.err ## error log file
+#SBATCH --mem=128G
+
+source ~/miniconda3/bin/activate spipev1
+
+split-pipe \
+	--mode all \
+	--kit WT \
+	--chemistry v1 \
+	--genome_dir ../../../genome/mm10 \
+	--fq1 ../../fastq/HC_2kA_R1.fastq.gz \
+	--fq2 ../../fastq/HC_2kA_R2.fastq.gz \
+	--output_dir ../../analysis_2B/ \
+	--sample HC_10_F_1 A1-A2 \
+	--sample HC_10_F_2 B1-B2 \
+	--sample HC_10_M_1 C1-C2 \
+	--sample HC_10_M_2 D1-D2 \
+	--sample HC_14_F_1 A3-A4 \
+	--sample HC_14_F_2 B3-B4 \
+	--sample HC_14_M_1 C3-C4 \
+	--sample HC_14_M_2 D3-D4 \
+	--sample HC_25_F_1 A5-A6 \
+	--sample HC_25_F_2 B5-B6 \
+	--sample HC_25_M_1 C5-C6 \
+	--sample HC_25_M_2 D5-D6 \
+	--sample HC_36_F_1 A7-A8 \
+	--sample HC_36_F_2 B7-B8 \
+	--sample HC_36_M_1 C7-C8 \
+	--sample HC_36_M_2 D7-D8 \
+	--sample HC_2m_F_1 A9-A10 \
+	--sample HC_2m_F_2 B9-B10 \
+	--sample HC_2m_M_1 C9-C10 \
+	--sample HC_2m_M_2 D9-D10 \
+	--sample HC_18m_M_1 A11-A12,C11-C12 \
+	--sample HC_18m_M_2 B11-B12,D11-D12 
